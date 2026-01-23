f31c7cab9a4e1ecca826e4ca0e5c4b84fe392196
pitchingcoachu <jgaynor@pitchingcoachu.com>
2026-01-23 16:24:58 -0700

    Update deploy_script.R

--- deploy_script.R
+++ deploy_script.R
@@ -59,7 +59,7 @@ deploy_app <- function() {
     cat("Deploying to shinyapps.io...\n")
     deployApp(
       appDir = ".",
-      appName = "baylorbaseball",
+      appName = "gmubaseball",
       forceUpdate = TRUE,
       launch.browser = FALSE,
       logLevel = "verbose"
