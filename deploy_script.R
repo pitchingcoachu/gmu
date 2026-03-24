09798c2871dc11ec202325f0811c73501e69b149
pitchingcoachu <jgaynor@pitchingcoachu.com>
2026-03-23 22:48:01 -0700

    Change appName from 'lsubaseball' to 'gmubaseball'

--- deploy_script.R
+++ deploy_script.R
@@ -90,7 +90,7 @@ deploy_app <- function() {
     deployApp(
       appDir = ".",
       appFiles = keep,
-      appName = "lsubaseball",
+      appName = "gmubaseball",
       forceUpdate = TRUE,
       launch.browser = FALSE,
       logLevel = "verbose"
